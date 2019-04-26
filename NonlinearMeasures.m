e7a8199ab3c2917903d2f496c6d5a7c61930d88d
ChanwoongYoon <y122396@gmail.com>
2019-04-27 01:47:35 +0900

    Update NonlinearMeasures.m

--- NonlinearMeasures.m
+++ NonlinearMeasures.m
@@ -1,8 +1,8 @@
-%clear;
+clear;
 close all;
 
 %% Load variables
-[DIR,TITLE] = fullDir_Xmat(1,3,2,0);
+[DIR,TITLE] = fullDir_Xmat(2,1,2,1);
 
 a = load(DIR);
 Xmat = a.Xmat;
@@ -12,14 +12,14 @@ dt = a.dt;
 rVec=0:100:1900;
 
 CVec = CorrelationDimension(Xmat, rVec);
-D2Vec = log(CVec)./log(rVec);
+D2Vec = diff(log(CVec))./diff(log(rVec));
+D2 = nanmax(D2Vec);
 
 figure;
 plot(log(rVec), log(CVec))
 xlabel('log r')
 ylabel('log C(r)')
-title(['D2: ',TITLE])
-fprintf("D2: %f\n", D2Vec(10))
+title([TITLE,  'D2: ',])
 
 %% Nonlinear measure: largest lyapunov exponent
 maxiter=size(Xmat,2)/50;
@@ -31,5 +31,5 @@ figure;
 plot(evolutionTime, d);
 xlabel('iteration')
 ylabel('divergence')
-title(['Lyapunov Exp: ',TITLE]) 
+title([TITLE, ' Lyapunov Exp: ',num2str(lle)]) 
 fprintf("largest lyapunov exponent %f\n", lle);

787870e91e8007c56a5b7326cf90095d6169c8f9
ChanWoong-Yoon <37400279+ChanWoong-Yoon@users.noreply.github.com>
2019-04-28 17:03:45 +0900

    Another version of figures

---
+++ NonlinearMeasures_all_at_once.m
@@ -0,0 +1,46 @@
+clear;
+close all;
+
+for idxClass = 1:2
+    for idxName = 1:3
+        for idxChannel = 1:2
+            for isSurrogate = 0:1
+                % Load variables
+                [DIR,TITLE] = fullDir_Xmat(idxClass,idxName,idxChannel,isSurrogate);
+
+                a = load(DIR);
+                Xmat = a.Xmat;
+                dt = a.dt;
+
+                % Nonlinear measure: correlation dimension
+                rVec=0:100:1900;
+
+                CVec = CorrelationDimension(Xmat, rVec);
+                D2Vec = diff(log(CVec))./diff(log(rVec));
+                D2 = nanmax(D2Vec);
+
+                fig_D2 = figure;
+                plot(log(rVec), log(CVec));
+                xlabel('log r')
+                ylabel('log C(r)')
+                title([TITLE,  ', D2: ', num2str(D2)])
+                savefig(fig_D2,['figure1/',TITLE,'_D2']);
+
+                % Nonlinear measure: largest lyapunov exponent
+                maxiter=size(Xmat,2)/50;
+                evolutionTime = (1:maxiter) * dt ;
+
+                [d, lle]= LyapunovExponent(Xmat, 2, maxiter, 1/dt, 1);
+
+                fig_Lya = figure;
+                plot(evolutionTime, d);
+                xlabel('iteration')
+                ylabel('divergence')
+                title([TITLE, ' Lyapunov Exp: ',num2str(lle)])
+                savefig(fig_Lya,['figure1/',TITLE,'_Lya']);
+            end
+        end
+    end
+end
+
+                
